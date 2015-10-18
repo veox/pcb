7a08a3306723bc209b5cedc7d92ef76f47b5f3c9
Britton Leo Kerin <britton.kerin@gmail.com>
2015-10-18 10:54:26 -0800

    Better docs, formatting

--- src/backtrace.h
+++ src/backtrace.h
@@ -1,5 +1,5 @@
-// Provide a backtrace with line numbers, and assertions that print this
-// backtrace when they fail.
+// Provide a backtrace with line numbers, and an assert()-like routine that
+// prints a backtrace on failure.
 
 #include <stdlib.h>
 
@@ -7,8 +7,17 @@
 // with line numbers.  Caveats:
 //
 //   * This uses the GNU libc backtrace() function, all caveats related to
-//     that function apply here as well.
+//     that function apply here as well.  In particular, the GCC -g compile
+//     time and -rdynamic link-time options are required, and line numbers
+//     might be wrong if the -O0 (disable all optimization) isn't used.
+//     There might be other issues as well.  To avoid these problems build
+//     pcb like this:
 //
+//       make CFLAGS='-Wall -g -O0'
+//
+//   * FIXME: as of this writing, --disable-nls is also required at
+//     ./configure-time to avoid bugs in NLS that trigger with -O0
+//     
 //   * It uses malloc(), so cannot reliably be used when malloc() might fail
 //     (e.g. after memory corruption).
 //
@@ -19,8 +28,8 @@ backtrace_with_line_numbers (void);
 
 // FIXME: so how does libc assert get the program name? test all this
 
-// Like assert(), but prints a full backtrace.  All caveats of
-// backtrace_with_line_numbers() apply.
+// Like assert(), but prints a full backtrace (if it does anything).
+// All caveats of backtrace_with_line_numbers() apply.
 #ifdef NDEBUG
 #  define ASSERT_BT(CONDITION)
 #else
@@ -28,7 +37,8 @@ backtrace_with_line_numbers (void);
     do {                                                                   \
       if ( __builtin_expect (!(CONDITION), 0) ) {                          \
         fprintf (                                                          \
-            stderr, "Assertion ` " #CONDITION "' failed.  Backtrace:\n%s", \
+            stderr,                                                        \
+            "Assertion ` " #CONDITION "' failed.  Backtrace:\n%s",         \
             backtrace_with_line_numbers() );                               \
         abort ();                                                          \
       }                                                                    \
