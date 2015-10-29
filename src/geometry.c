ab1b39b5cd1f5b319fed73d9f0b0084a9de6125f
Britton Leo Kerin <britton.kerin@gmail.com>
2015-10-28 18:38:15 -0800

    More methods and types

--- src/geometry.c
+++ src/geometry.c
@@ -1,5 +1,10 @@
 // Interface of the implementation described in geometry.h.
 
+#define _GNU_SOURCE
+
+#include <assert.h>
+#include <math.h>
+
 #include "geometry.h"
 
 double
@@ -45,6 +50,7 @@ vec_proj (Vec va, Vec vb)
   return vec_scale (vb, ((double) vec_dot (va, vb)) / vec_dot (vb, vb));
 }
 
+// FIXME: this should be vec_add() since it only supports two arguments
 Vec
 vec_sum (Vec va, Vec vb)
 {
@@ -54,6 +60,30 @@ vec_sum (Vec va, Vec vb)
   result.y = va.y + vb.y;
 
   return result;
+}
+
+double
+normalize_angle_in_radians (double angle)
+{
+  while ( angle < 0.0 ) {
+    angle += 2.0 * M_PI;
+  }
+  while ( angle >= 2.0 * M_PI ) {
+    angle -= 2.0 * M_PI;
+  }
+
+  return angle;
+}
+
+bool
+angle_in_span (double theta, double start_angle, double angle_delta)
+{
+  if ( angle_delta > 0.0 ) {
+    return angle_delta >= normalize_angle_in_radians (theta - start_angle);
+  }
+  else {
+    return -angle_delta >= normalize_angle_in_radians (-theta + start_angle);
+  }
 }
 
 // FIXME: I don't do anything efficient for horizontal/vertical line segments
@@ -97,6 +127,172 @@ nearest_point_on_line_segment (Vec pt, LineSegment const *seg)
   else {
     return pp;       // Projection landed on line, so projected point
   }
+}
+
+// FIXME: change the old_nearest_point_on_arc to use this new one
+
+Vec
+nearest_point_on_arc (Vec pt, Arc2 *arc)
+{
+  Vec cent = arc->circle.center;
+  Coord rad = arc->circle.radius;
+
+  // Translate pt st Arc circle is centered at origin wrt it
+  pt.x -= cent.x;
+  pt.y -= cent.y;
+
+  double sa = arc->start_angle;
+  double ad = arc->angle_delta;
+  double ea = sa + ad;   // End Angle.   Note: in [0, 4.0 * M_PI)
+
+  // Nearest Point on Circle
+  Vec npoc = vec_scale (pt, rad / vec_mag (pt));
+
+  double theta_npoc = atan2 (npoc.y, npoc.x);
+
+  Vec result;
+
+  // If the nearest point on the underlying circle is part of the arc,
+  // the point itself is the result we want, otherwise the end point of
+  // the arc closest to the nearest point is the one we want.
+  if ( angle_in_span (theta_npoc, sa, ad) ) {
+    result = npoc;
+  }
+  else {
+    // FIXME: use sincos() for speed and deredundification here
+    // End Point A.  round() probably isn't really worth it here
+    Vec epa = { round (rad * cos (sa)), round (rad * sin (sa)) };
+    // End Point B   round() probably isn't really worth it here
+    Vec epb = { round (rad * cos (ea)), round (rad * sin (ea)) };
+    double m_npoc_epa = vec_mag (vec_from (npoc, epa));
+    double m_npoc_epb = vec_mag (vec_from (npoc, epb));
+    result  = (m_npoc_epa < m_npoc_epb ? epa : epb);
+  }
+
+  // Translate back to true position
+  result.x += cent.x;
+  result.y += cent.y;
+
+  return result;
+}
+
+int
+circle_line_segment_intersection (
+    Circle const *circ,
+    LineSegment const *ls,
+    Vec intersection[2] )
+{
+  // Translated end point coordinates st circ is relatively situated at (0, 0).
+  Coord x1 = ls->pa.x - circ->center.x;
+  Coord y1 = ls->pa.y - circ->center.y;
+  Coord x2 = ls->pb.x - circ->center.x;
+  Coord y2 = ls->pb.y - circ->center.y;
+
+  // Degenerate cases. We can't get these right in general, and clients have
+  // been warned.
+  assert (! ((x1 == x2 && y1 == y2) || (circ->radius == 0)));
+    
+  // Straight from http://mathworld.wolfram.com/Circle-LineIntersection.html
+  Coord dx = x2 - x1;
+  Coord dy = y2 - y1;
+  double dr = hypot (dx, dy);
+  double determinant = ((double) x1) * y2 - ((double) x2) * y1;
+  double dr2 = pow (dr, 2.0);
+  double discriminant = pow (circ->radius, 2.0) * dr2 - pow (determinant, 2.0);
+  if ( discriminant < 0 ) {
+    return 0;   // We aren't interested in imaginary intersections
+  }
+  double sqrt_disc = sqrt (discriminant);
+  double temp = (dy < 0 ? -1.0 : 1.0) * dx * sqrt_disc;
+  Coord ip1x = round ((determinant * dy + temp) / dr2);
+  Coord ip2x = round ((determinant * dy - temp) / dr2);
+  temp = fabs (dy) * sqrt_disc;
+  Coord ip1y = round ((-determinant * dx + temp) / dr2);
+  Coord ip2y = round ((-determinant * dx - temp) / dr2);
+  
+  int result = 0;
+
+  // FIXME: I think the below mess is no longer worth it and we should
+  // instead add vectors and check if they get bigger or smaller.
+
+  // Now we just need to check which of the intersections are in our segment.
+  // We only need to check one coordinate, since the intersection points are
+  // known to be on the (infinite) underlying geomatrical line.  However,
+  // because ip1x and ip2x are tainted by floating point and our lines are
+  // likely to be parallel to the axes, we make sure to check the wider span.
+  if ( abs (dx) >= abs (dy) ) {
+    Coord lx = ( x1 < x2 ? x1 : x2);   // Lower x
+    Coord hx = ( x1 < x2 ? x2 : x1);   // Higher x
+    if ( lx <= ip1x && ip1x <= hx ) {
+      if ( intersection != NULL ) {
+        intersection[result]
+          = ((Vec) { ip1x + circ->center.x, ip1y + circ->center.y });
+      }
+      result++;
+    }
+    if ( lx <= ip2x && ip2x <= hx ) {
+      if ( intersection != NULL ) {
+        intersection[result]
+          = ((Vec) { ip2x + circ->center.x, ip2y + circ->center.y });
+      }
+      result++;
+    }
+  }
+  else {
+    Coord ly = ( y1 < y2 ? y1 : y2);   // Lower y
+    Coord hy = ( y1 < y2 ? y2 : y1);   // Higher y
+    if ( ly <= ip1y && ip1y <= hy ) {
+      if ( intersection != NULL ) {
+        intersection[result]
+          = ((Vec) { ip1x + circ->center.x, ip1y + circ->center.y });
+      }
+      result++;
+    }
+    if ( ly <= ip2y && ip2y <= hy ) {
+      if ( intersection != NULL ) {
+        intersection[result]
+          = ((Vec) { ip2x + circ->center.x, ip2y + circ->center.y });
+      }
+      result++;
+    }
+  }
+  
+  return result;
+}
+
+int
+arc_line_segment_intersection (
+    Arc2 const *arc,
+    LineSegment const *ls,
+    Vec intersection[2] )
+{
+  // Underlying Cicle.  FIXME: change Arc type to reference a circle so this
+  // can go away.
+  Circle const *uc = &(arc->circle);
+   
+  // Get intersection count and intersection points 
+  Vec ci[2];   // Circle Intersection (up to two points)
+  int cic;     // Circle Intersection Count
+  cic = circle_line_segment_intersection (uc, ls, ci);
+
+  int result = 0;
+
+  while ( cic > 0 ) {
+    bool iioa   // Intersection Is On Arc
+      = angle_in_span (
+          atan2 (ci[cic - 1].y - uc->center.y, ci[cic - 1].x - uc->center.x),
+          arc->start_angle,
+          arc->angle_delta );
+    if ( iioa ) {
+      if ( intersection != NULL ) {
+        intersection[result] = ci[cic - 1];
+      }
+      result++;
+    }
+    cic--;
+  }
+
+  return result;
 }
 
 bool
@@ -228,4 +424,43 @@ circle_intersects_rectangle (
       circle_intersects_line_segment (circle, &c2_c3, pii) ||
       circle_intersects_line_segment (circle, &c3_c4, pii) ||
       circle_intersects_line_segment (circle, &c4_c1, pii) );
+}
+
+bool
+circle_intersects_rectangle_2 (Circle const *circ, Rectangle *rect, Vec *pii)
+{
+  // Line segments between corners of rectangle
+  LineSegment c1_c2 = { rect->c1, rect->c2 };
+  LineSegment c2_c3 = { rect->c2, rect->c3 };
+  LineSegment c3_c4 = { rect->c3, rect->c4 };
+  LineSegment c4_c1 = { rect->c4, rect->c1 };
+
+  // Check if the center of the circle is on the rectangle.  This catches
+  // the situation where the circle is entirely inside the rectangle.
+  if ( point_is_on_rectangle (circ->center, rect) ) {
+    *pii = circ->center;
+    return true;
+  }
+
+  // Note that pii (if not NULL) is computed by the first short-circuit true
+  // result here.
+  return (
+      circle_intersects_line_segment (circ, &c1_c2, pii) ||
+      circle_intersects_line_segment (circ, &c2_c3, pii) ||
+      circle_intersects_line_segment (circ, &c3_c4, pii) ||
+      circle_intersects_line_segment (circ, &c4_c1, pii) );
+}
+
+void
+arc_end_points (Arc2 *arc, Vec ep[2])
+{
+  // Sines and cosines of Start Angle/End Angle
+  double sin_sa, cos_sa, sin_ea, cos_ea;
+  sincos (arc->start_angle, &sin_sa, &cos_sa);
+  sincos (arc->start_angle + arc->angle_delta, &sin_ea, &cos_ea);
+
+  double radius = arc->circle.radius;
+
+  ep[0] = ((Vec) { round (radius * cos_sa), round (radius * sin_sa) });
+  ep[1] = ((Vec) { round (radius * cos_ea), round (radius * sin_ea) });
 }
