f48486475a480fd865112657f6c26af1c6503fc5
Britton Leo Kerin <britton.kerin@gmail.com>
2015-11-02 16:21:58 -0900

    Factor into draw_helpers

--- src/hid/common/draw_helpers.h
+++ src/hid/common/draw_helpers.h
@@ -1,3 +1,12 @@
+
+// Support for constant-pixel size debug markers
+#define DEBUG_MARKER_RADIUS_PIXELS 8
+#define MAX_DEBUG_MARKER_COUNT 1042
+typedef struct {
+  Coord x, y;
+} DebugMarker;
+extern DebugMarker debug_markers[MAX_DEBUG_MARKER_COUNT];
+
 void common_gui_draw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box);
 void common_fill_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box);
 void common_thindraw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box);
